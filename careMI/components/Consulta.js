414c5027ec7d80bd9e0f62f7c1a233c2edab1037
nicollyADS <rm552579@fiap.com.br>
2024-05-13 07:15:54 -0300

    criando componentes

---
+++ careMI/components/Consulta.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Consulta = () => {
+  return (
+    <View>
+      <Text>Consulta</Text>
+    </View>
+  )
+}
+
+export default Consulta;
---
+++ careMI/components/Exames.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Exame = () => {
+  return (
+    <View>
+      <Text>Exame</Text>
+    </View>
+  )
+}
+
+export default Exame;

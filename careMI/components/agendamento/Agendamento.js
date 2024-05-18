7c4fae4aea6d3f32cee5741213287cd7d1ff31b5
nicollyADS <rm552579@fiap.com.br>
2024-05-18 04:41:44 -0300

    fazendo o post do agendamento e criando a flatlist para o atendimento

--- careMI/components/agendamento/Agendamento.js
+++ careMI/components/agendamento/Agendamento.js
@@ -1,37 +1,116 @@
-import { View, Text, Touchable, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const Agendamento = () => {
+
+
+const Agendamento = ( ) => {
+  const [dias, setDias] = useState('');
+  const [habito, setHabito] = useState('');
+  const [tempoSono, setTempoSono] = useState('');
+  const [hereditario, setHereditario] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [dataEnvio, setDataEnvio] = useState('');
+
+  const navigation = useNavigation();
+
+
+  const cadastrar = async (dadosFormulario) => {
+    try {
+      const response = await fetch('http://localhost:8080/atendimentos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosFormulario)
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao enviar os dados do agendamento');
+      }
+
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      console.error('Erro ao cadastrar:', error);
+      throw error;
+    }
+  };
+
+
+
+  const handleSubmit = () => {
+    const dadosFormulario = {
+      dias,
+      habito,
+      tempoSono,
+      hereditario,
+      descricao,
+      dataEnvio
+    };
+
+    cadastrar(dadosFormulario)
+    .then((responseData) => {
+      navigation.navigate('Atendimento', { dados: dadosFormulario });
+    })
+    .catch((error) => {
+      console.error('Erro ao cadastrar:', error);
+    });
+
+
+  };
+
   return (
-    <View>
+    <View >
       <Text>Agendamento</Text>
 
       <Text>Quantos dias com os sintomas? *</Text>
-      <TextInput placeholder='Informe os dias em numero (ex: 3)'/>
+      <TextInput
+        placeholder='Informe os dias em número (ex: 3)'
+        value={dias}
+        onChangeText={setDias}
+      />
 
       <Text>Os sintomas são recorrentes? *</Text>
-      <TextInput placeholder='Informe se esses sintomas são comuns' />
+      <TextInput
+        placeholder='Informe se esses sintomas são comuns'
+        value={habito}
+        onChangeText={setHabito}
+      />
 
       <Text>Quanto tempo de sono por dia? *</Text>
-      <TextInput placeholder='Informe as suas horas diárias de sono' />
+      <TextInput
+        placeholder='Informe as suas horas diárias de sono'
+        value={tempoSono}
+        onChangeText={setTempoSono}
+      />
 
-      <Text>Possui doenças herediárias? *</Text>
-      <TextInput  placeholder='Informe quais doenças possui' />
+      <Text>Possui doenças hereditárias? *</Text>
+      <TextInput
+        placeholder='Informe quais doenças possui'
+        value={hereditario}
+        onChangeText={setHereditario}
+      />
 
       <Text>Descrição dos sintomas *</Text>
-      <TextInput placeholder='Informe detalhadamente os seus sintomas '/>
+      <TextInput
+        placeholder='Informe detalhadamente os seus sintomas'
+        value={descricao}
+        onChangeText={setDescricao}
+      />
 
       <Text>Data e hora do agendamento *</Text>
-      <TextInput placeholder='Informe data e hora para agendar'/>
+      <TextInput
+        placeholder='Informe data e hora para agendar'
+        value={dataEnvio}
+        onChangeText={setDataEnvio}
+      />
 
-      {/* <TouchableOpacity  onPress={() => {
-        }}>
-            <Text>ENVIAR</Text>
-        </TouchableOpacity> */}
-
-    
+      <TouchableOpacity onPress={handleSubmit}>
+        <Text>ENVIAR</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 export default Agendamento;
--- careMI/components/atendimento/Atendimento.js
+++ careMI/components/atendimento/Atendimento.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 const Atendimento = () => {
+  const route = useRoute();
+  const { dados } = route.params;
+  
+
+  const dadosArray = [
+    { key: 'Dias', value: dados.dias },
+    { key: 'Hábito', value: dados.habito },
+    { key: 'Sono', value: dados.tempoSono },
+    { key: 'Hereditario', value: dados.hereditario },
+    { key: 'Descrição', value: dados.descricao },
+    { key: 'Data', value: dados.dataEnvio }
+  ];
+
   return (
     <View>
       <Text>Atendimento</Text>
+      <FlatList
+        data={dadosArray}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.key}</Text>
+            <Text>{item.value}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.key}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default Atendimento;

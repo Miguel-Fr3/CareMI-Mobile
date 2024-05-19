b4c40006a32aac7a43197b384f17ab2b43678d4a
nicollyADS <rm552579@fiap.com.br>
2024-05-19 03:27:20 -0300

    fazendo o login

--- careMI/App.js
+++ careMI/App.js
@@ -1,8 +1,7 @@
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
 import Login from './components/login/Login'
 import Menu from './components/menu/Menu';
-import { useState } from 'react'
-
 
 export default function App() {
   const [logado, setLogado] = useState(false);
@@ -17,4 +16,3 @@ export default function App() {
       </View>
   );
 } 
-
--- careMI/components/login/Login.js
+++ careMI/components/login/Login.js
@@ -3,10 +3,12 @@ import { Text, TextInput, View, TouchableOpacity, Modal } from 'react-native';
 import { style } from './style';
 import EsqueciSenha from '../senha/EsqueciSenha';
 
-const Login = (props) => {
+const Login = ({ logar }) => {
   const [cpf, setCpf] = useState("");
   const [senha, setSenha] = useState("");
-  const [modalEsqueciSenhaVisible, setModalEsqueciSenhaVisible] = useState(false); 
+  const [modalEsqueciSenhaVisible, setModalEsqueciSenhaVisible] = useState(false);
+  const [erroLogin, setErroLogin] = useState("");
+
 
   const abrirModalEsqueciSenha = () => {
     setModalEsqueciSenhaVisible(true);
@@ -15,7 +17,26 @@ const Login = (props) => {
   const fecharModalEsqueciSenha = () => {
     setModalEsqueciSenhaVisible(false);
   };
-  
+
+  const handleLogin = () => {
+    if (cpf && senha) {
+      fetch("http://localhost:8080/logins")
+        .then(response => response.json())
+        .then(data => {
+          const loginEncontrado = data.find(login => login.cpf === cpf && login.senha === senha);
+          if (loginEncontrado) {
+            console.log("login encontrado")
+            logar();
+          } else {
+            setErroLogin("CPF ou senha inválidos");
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao buscar lista de logins:', error);
+          setErroLogin('Ocorreu um erro ao realizar o login. Por favor, tente novamente mais tarde.');
+        });
+    }
+  };
 
   return (
     <View style={style.container}>
@@ -31,26 +52,23 @@ const Login = (props) => {
         <TextInput value={senha} onChangeText={setSenha} style={style.input} secureTextEntry={true} />
       </View>
 
-      <TouchableOpacity style={style.button} onPress={() => {
-        props.logar();
-      }}>
+      <TouchableOpacity style={style.button} onPress={handleLogin}>
         <Text style={{ color: '#fbfbfb', fontSize: "1rem", fontWeight: '600' }}>Entrar</Text>
       </TouchableOpacity>
 
-    
+      <Text>{erroLogin}</Text>
       <TouchableOpacity onPress={abrirModalEsqueciSenha}>
         <Text style={{ color: '#fbfbfb', fontSize: "0.9rem", fontWeight: '600', marginTop: '1rem', textDecorationLine: 'underline' }}>
           Esqueci minha senha
         </Text>
       </TouchableOpacity>
 
-     
       <Modal
         visible={modalEsqueciSenhaVisible}
         onRequestClose={fecharModalEsqueciSenha}
       >
         <View>
-          <EsqueciSenha  fecharModalEsqueciSenha={fecharModalEsqueciSenha}/>
+          <EsqueciSenha fecharModalEsqueciSenha={fecharModalEsqueciSenha} />
         </View>
       </Modal>
     </View>

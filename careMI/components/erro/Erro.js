c3b2541503de8fd3380ba71910a892155be4ec7a
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-05-19 15:43:37 -0300

    Tela de erros

--- careMI/components/erro/Erro.js
+++ careMI/components/erro/Erro.js
@@ -1,13 +1,14 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import Error from '../../assets/images/error.png'
+import { style } from './style'
 
 
 const Erro = () => {
 
     return(
-        <View>
-            <Text>OOPS! não foi possível concluir sua solicitação</Text>
+        <View style={style.errorContainer}>
+            <Text style={style.errorTittle}>OOPS! não foi possível concluir sua solicitação</Text>
             <Image source={Error} style={{ width: 100, height: 100 }} ></Image>
         </View>
     )

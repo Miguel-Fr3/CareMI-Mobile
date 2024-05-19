2655eca036a6d7736343597bd42d586300ebb3ee
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-05-19 18:14:18 -0300

    Agendamento

--- careMI/components/agendamento/Agendamento.js
+++ careMI/components/agendamento/Agendamento.js
@@ -3,6 +3,7 @@ import { View, Text, TextInput, TouchableOpacity, Modal } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as yup from 'yup';
 import SucessoAgendamento from './SucessoAgendamento';
+import { style } from './style';
 
 const schema = yup.object().shape({
   dias: yup.string().required('campo obrigatório.'),
@@ -72,59 +73,72 @@ const Agendamento = () => {
   };
 
   return (
-    <View>
-      <Text>Agendamento</Text>
+    <View style={style.container}>
+      <View style={style.inputLabel}>
+        <Text style={style.label}>Quantos dias com os sintomas? *</Text>
+        <TextInput
+          style={style.input}
+          placeholder='Informe os dias em número (ex: 3)'
+          value={dias}
+          onChangeText={setDias}
+        />
+        {errors.dias && <Text style={{color:'red'}} >{errors.dias}</Text>}
+      </View>
 
-      <Text>Quantos dias com os sintomas? *</Text>
-      <TextInput
-        placeholder='Informe os dias em número (ex: 3)'
-        value={dias}
-        onChangeText={setDias}
-      />
-      {errors.dias && <Text style={{color:'red'}} >{errors.dias}</Text>}
+      <View>
+        <Text style={style.label}>Os sintomas são recorrentes? *</Text>
+        <TextInput style={style.input}
+          placeholder='Informe se esses sintomas são comuns'
+          value={habito}
+          onChangeText={setHabito}
+        />
+        {errors.habito && <Text style={{color:'red'}} >{errors.habito}</Text>}
+      </View>
 
-      <Text>Os sintomas são recorrentes? *</Text>
-      <TextInput
-        placeholder='Informe se esses sintomas são comuns'
-        value={habito}
-        onChangeText={setHabito}
-      />
-      {errors.habito && <Text style={{color:'red'}} >{errors.habito}</Text>}
+      <View>
+        <Text style={style.label}>Quanto tempo de sono por dia? *</Text>
+        <TextInput style={style.input}
+          placeholder='Informe as suas horas diárias de sono'
+          value={tempoSono}
+          onChangeText={setTempoSono}
+        />
+        {errors.tempoSono && <Text style={{color:'red'}} >{errors.tempoSono}</Text>}
+      </View>
 
-      <Text>Quanto tempo de sono por dia? *</Text>
-      <TextInput
-        placeholder='Informe as suas horas diárias de sono'
-        value={tempoSono}
-        onChangeText={setTempoSono}
-      />
-      {errors.tempoSono && <Text style={{color:'red'}} >{errors.tempoSono}</Text>}
 
-      <Text>Possui doenças hereditárias? *</Text>
-      <TextInput
-        placeholder='Informe quais doenças possui'
-        value={hereditario}
-        onChangeText={setHereditario}
-      />
-      {errors.hereditario && <Text style={{color:'red'}} >{errors.hereditario}</Text>}
+      <View>
+        <Text style={style.label}>Possui doenças hereditárias? *</Text>
+        <TextInput style={style.input}
+          placeholder='Informe quais doenças possui'
+          value={hereditario}
+          onChangeText={setHereditario}
+        />
+        {errors.hereditario && <Text style={{color:'red'}} >{errors.hereditario}</Text>}
+      </View>
 
-      <Text>Descrição dos sintomas *</Text>
-      <TextInput
-        placeholder='Informe detalhadamente os seus sintomas'
-        value={descricao}
-        onChangeText={setDescricao}
-      />
-      {errors.descricao && <Text style={{color:'red'}} >{errors.descricao}</Text>}
+      <View>
+        <Text style={style.label}>Descrição dos sintomas *</Text>
+        <TextInput style={style.input}
+          placeholder='Informe detalhadamente os seus sintomas'
+          value={descricao}
+          onChangeText={setDescricao}
+        />
+        {errors.descricao && <Text style={{color:'red'}} >{errors.descricao}</Text>}
+      </View>
 
-      <Text>Data e hora do agendamento *</Text>
-      <TextInput
-        placeholder='Informe data e hora para agendar'
-        value={dataEnvio}
-        onChangeText={setDataEnvio}
-      />
-      {errors.dataEnvio && <Text style={{color:'red'}} >{errors.dataEnvio}</Text>}
+      <View>
+        <Text style={style.label}>Data e hora do agendamento *</Text>
+        <TextInput style={style.input}
+          placeholder='Informe data e hora para agendar'
+          value={dataEnvio}
+          onChangeText={setDataEnvio}
+        />
+        {errors.dataEnvio && <Text style={{color:'red'}} >{errors.dataEnvio}</Text>}
+      </View>
 
-      <TouchableOpacity onPress={handleSubmit}>
-        <Text>ENVIAR</Text>
+
+      <TouchableOpacity style={style.button} onPress={handleSubmit}>
+        <Text style={style.text}>ENVIAR</Text>
       </TouchableOpacity>
 
       <Modal

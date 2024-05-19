64f0345934e9470601c41f4e866e84777d8defc9
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-05-19 17:25:25 -0300

    Finalizando home

--- careMI/components/home/Home.js
+++ careMI/components/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { style } from './style';
+import { Image } from 'react-native';
 const Home = () => {
   const navigation = useNavigation();
 
@@ -10,21 +11,27 @@ const Home = () => {
       <Text style={style.tittle}>Olá, Bem Vindo!</Text>
 
       <TouchableOpacity style={style.button} onPress={() => navigation.navigate('Exames')}>
-        <Image source={require('../../assets/icon/exames.svg')} />
-        <Text>Acessar exames</Text>
+        <View style={style.icon}>
+          <Image style={{backgroundColor: '#008080', width: '44.5px', height: '50px'}} source={require('../../assets/icon/exames.svg')} />
+        </View>
+        <Text style={style.text}>Acessar exames</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={style.button} onPress={() => navigation.navigate('Agendamento')}>
-        <Image source={require('../../assets/icon/agendamento.svg')} />
-        <Text>Agendamento Online</Text>
+        <View style={style.icon}>
+          <Image style={{backgroundColor: '#008080', width: '44.5px', height: '50px'}} source={require('../../assets/icon/agendamento.svg')} />
+        </View>
+        <Text style={style.text}>Agendamento Online</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={style.button} onPress={() => navigation.navigate('Atendimento')}>
-        <Image source={require('../../assets/icon/atendimento.svg')} />
-        <Text>Atendimento</Text>
+        <View style={style.icon}>
+          <Image style={{backgroundColor: '#008080', width: '44.5px', height: '50px'}} source={require('../../assets/icon/atendimento.svg')} />
+        </View> 
+        <Text style={style.text}>Atendimento</Text>
       </TouchableOpacity>
 
-      <Text>© 2024 CareMI, Inc.</Text>
+      <Text style={{marginTop: '2rem', marginBottom: '6rem'}}>© 2024 CareMI, Inc.</Text>
     </View>
   );
 };
--- careMI/components/home/style.js
+++ careMI/components/home/style.js
@@ -2,7 +2,47 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 
 export const style = StyleSheet.create({
+    container: {
+        backgroundColor: '#fbfbfb',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tittle: {
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        marginTop: '3rem',
+        marginBottom: '3rem',
+        textAlign: 'center',
+        color: '#008080',
+    },
+    button: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'row',
+        width: '20rem',
+        marginLeft: '1rem',
+        marginBottom: '4rem',
 
+    },
+    icon: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#008080',
+        padding: '1rem',
+        borderRadius: '3rem',
+        marginRight: '1rem',
+    },
+    text: {
+        fontSize: '1rem',
+        width: '130px',
+        marginLeft: '1rem',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#008080',
+    }
 });
 
 
